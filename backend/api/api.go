b846afb00d17825539b10f4538c1363ca9914908
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-11-30 01:47:27 +0530

    add: health route

--- backend/api/api.go
+++ backend/api/api.go
@@ -3,12 +3,16 @@ package api
 import (
 	"net/http"
 
+	"github.com/Shobhit-Nagpal/badgeflow/backend/api/handler"
 	"github.com/Shobhit-Nagpal/badgeflow/backend/internal/config"
 	"github.com/Shobhit-Nagpal/badgeflow/backend/internal/database"
 )
 
 func NewServer(cfg *config.Config, queries *database.Queries) *http.Server {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /api/healthz", handler.Health)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    cfg.Addr,
--- backend/api/handler/handler.go
+++ backend/api/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"io"
+	"net/http"
+)
+
+
+func Health(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-9")
+	io.WriteString(w, "OK")
+	w.WriteHeader(http.StatusOK)
+}
