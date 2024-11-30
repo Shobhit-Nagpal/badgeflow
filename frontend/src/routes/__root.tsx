321862eead1b90d0b67a0491032a11ba9fe21665
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-01 00:41:58 +0530

    add: sidebar + fix: tailwind install

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,20 +1,26 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import "@/index.css";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
+      <SidebarProvider>
+        <div className="p-2 flex gap-2">
+          <AppSidebar />
+          <SidebarInset>
+            <SidebarTrigger className="-ml-1" />
+            <Outlet />
+          </SidebarInset>
+        </div>
+        <TanStackRouterDevtools />
+      </SidebarProvider>
     </>
   ),
-})
+});
