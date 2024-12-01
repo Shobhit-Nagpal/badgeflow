8a69aca5865285ed0fde050c8de0a58375f85747
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-01 16:44:15 +0530

    init tanstack query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
 import { useAuth } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function App() {
   const auth = useAuth();
 
-  return <RouterProvider router={router} context={{ auth }} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ auth }} />;
+    </QueryClientProvider>
+  );
 }
