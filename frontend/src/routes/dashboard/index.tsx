68b3e22a4ec1961e525bd159a25aa92c5560f435
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-02 17:55:08 +0530

    add: max width on dashboard + remove vercel config on backend

--- frontend/src/routes/dashboard/index.tsx
+++ frontend/src/routes/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import { QuickActions } from "@/components/dashboard/quick-actions";
 import { TopStatsOverview } from "@/components/dashboard/top-stats-overview";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { useGetDashboardMetrics } from "@/hooks/use-clerk-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -23,14 +24,16 @@ export function Dashboard() {
   }
 
   return (
-    <div className="flex-1 space-y-4 p-8 pt-6 w-full">
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-        <div className="flex items-center space-x-2">
-          <QuickActions />
+    <MaxWidthWrapper>
+      <div className="flex-1 space-y-4 p-8 pt-6 w-full">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+          <div className="flex items-center space-x-2">
+            <QuickActions />
+          </div>
         </div>
+        <TopStatsOverview metrics={metrics} />
       </div>
-      <TopStatsOverview metrics={metrics} />
-    </div>
+    </MaxWidthWrapper>
   );
 }
